8f33c482aad7dc337dd33cbfcb391c5cd47b5cf8
arnab-ray <18110248+arnab-ray@users.noreply.github.com>
2020-12-25 23:08:36 +0530

    saving changes

---
+++ src/dynamicprogramming/medium/MaxPointFromCard.java
@@ -0,0 +1,22 @@
+package dynamicprogramming.medium;
+
+public class MaxPointFromCard {
+    public int maxScore(int[] cardPoints, int k) {
+        int[] leftSum = new int[k + 1];
+        int[] rightSum = new int[k + 1];
+        leftSum[0] = 0;
+        rightSum[0] = 0;
+
+        for (int i = 1; i <= k; i++)
+            leftSum[i] = leftSum[i - 1] + cardPoints[i - 1];
+
+        for (int i = 1; i <= k; i++)
+            rightSum[i] = rightSum[i - 1] + cardPoints[cardPoints.length - i];
+
+        int maxPoint = 0;
+        for (int i = 0; i <= k; i++)
+            maxPoint = Math.max(maxPoint, leftSum[i] + rightSum[k - i]);
+
+        return maxPoint;
+    }
+}
