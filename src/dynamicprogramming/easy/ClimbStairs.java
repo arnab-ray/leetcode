a9af0a0a5c0b31204e0f6195de9a7409733be207
arnab-ray <18110248+arnab-ray@users.noreply.github.com>
2021-09-18 21:17:42 +0530

    save changes

---
+++ src/dynamicprogramming/easy/ClimbStairs.java
@@ -0,0 +1,20 @@
+package dynamicprogramming.easy;
+
+public class ClimbStairs {
+    public int climbStairs(int n) {
+        if(n == 1)
+            return 1;
+        if(n == 2)
+            return 2;
+
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i = 3; i < n + 1; i++)
+            dp[i] = dp[i-1] + dp[i-2];
+
+        return dp[n];
+    }
+}
---
+++ src/dynamicprogramming/easy/TribonacciNumber.java
@@ -0,0 +1,4 @@
+package dynamicprogramming.easy;
+
+public class TribonacciNumber {
+}
