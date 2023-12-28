f137094482f1f2cd4d0698ccb801229ff21f0b54
Arnab Ray <arnab.ray@udaan.com>
2023-12-28 09:05:57 +0530

    save changes

---
+++ src/arrays/medium/LongestConsecutiveSequence.java
@@ -0,0 +1,39 @@
+package arrays.medium;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestConsecutiveSequence {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        Set<Integer> unvisited = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+            unvisited.add(num);
+        }
+
+        int maxLength = 0;
+        for (int num : nums) {
+            if (unvisited.contains(num)) {
+                unvisited.remove(num);
+                int currentLength = 1;
+                int prevVal = num - 1;
+                int nextVal = num + 1;
+                while (unvisited.contains(prevVal)) {
+                    unvisited.remove(prevVal);
+                    currentLength++;
+                    prevVal--;
+                }
+                while (unvisited.contains(nextVal)) {
+                    unvisited.remove(nextVal);
+                    currentLength++;
+                    nextVal++;
+                }
+
+                maxLength = Math.max(maxLength, currentLength);
+            }
+        }
+
+        return maxLength;
+    }
+}
