07f1576a952bb31b2afdd2b51d82e586bde98dab
Arnab Ray <arnab.ray@udaan.com>
2024-01-03 21:08:13 +0530

    optimise code

--- src/binarysearch/medium/FindFirstAndLast.java
+++ src/binarysearch/medium/FindFirstAndLast.java
@@ -2,37 +2,33 @@ package binarysearch.medium;
 
 public class FindFirstAndLast {
     private int getFirstPos(int[] nums, int low, int high, int target) {
-        if(low > high)
+        if (low > high)
             return -1;
-        if(low == high)
+        if (low == high)
             return nums[low] == target ? low : -1;
 
         int mid = low + (high - low)/2;
-        if(nums[mid] == target && (low == mid || (low < mid && nums[mid - 1] < nums[mid])))
+        if (nums[mid] == target && (low == mid || (low < mid && nums[mid - 1] < nums[mid])))
             return mid;
-        else if(nums[mid] == target)
-            return getFirstPos(nums, low, mid - 1, target);
-        else if (nums[mid] > target)
+        else if (nums[mid] >= target)
             return getFirstPos(nums, low, mid - 1, target);
         else
             return getFirstPos(nums, mid + 1, high, target);
     }
 
     private int getLastPos(int[] nums, int low, int high, int target) {
-        if(low > high)
+        if (low > high)
             return -1;
-        if(low == high)
+        if (low == high)
             return nums[low] == target ? low : -1;
 
         int mid = low + (high - low)/2;
-        if(nums[mid] == target && (mid == high || (mid < high && nums[mid] < nums[mid + 1])))
+        if (nums[mid] == target && (mid == high || (mid < high && nums[mid] < nums[mid + 1])))
             return mid;
-        else if(nums[mid] == target)
+        else if(nums[mid] <= target)
             return getLastPos(nums, mid + 1, high, target);
-        else if (nums[mid] > target)
-            return getLastPos(nums, low, mid - 1, target);
         else
-            return getLastPos(nums, mid + 1, high, target);
+            return getLastPos(nums, low, mid - 1, target);
     }
 
     public int[] searchRange(int[] nums, int target) {
