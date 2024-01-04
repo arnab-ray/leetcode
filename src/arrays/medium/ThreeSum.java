64573f7b435f1ada4a61bdb68da86b9273fc5a07
Arnab Ray <arnab.ray@udaan.com>
2024-01-04 20:24:21 +0530

    save changes

--- src/arrays/medium/ThreeSum.java
+++ src/arrays/medium/ThreeSum.java
@@ -25,4 +25,42 @@ public class ThreeSum {
 
         return new LinkedList<>(result);
     }
+
+    public static List<List<Integer>> threeSum2(int[] nums) {
+        Set<List<Integer>> result = new HashSet<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while (j < k) {
+                if (nums[i] + nums[j] + nums[k] == 0) {
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[j]);
+                    temp.add(nums[k]);
+                    result.add(temp);
+                    j++;
+                } else if (nums[i] + nums[j] + nums[k] > 0) {
+                    k--;
+                } else {
+                    j++;
+                }
+            }
+        }
+
+        return new ArrayList<>(result);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[] {-1,0,1,2,-1,-4};
+        List<List<Integer>> res = threeSum2(arr);
+
+        for (List<Integer> res_ : res) {
+            for (Integer i : res_) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
 }
