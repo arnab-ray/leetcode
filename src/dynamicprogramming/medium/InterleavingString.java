bd886949d725eff9d50a7d2c808a70aa46771cfd
Arnab Ray <arnab.ray@udaan.com>
2024-01-04 21:58:57 +0530

    save changes

--- src/dynamicprogramming/medium/InterleavingString.java
+++ src/dynamicprogramming/medium/InterleavingString.java
@@ -6,19 +6,19 @@ import java.util.Map;
 // #97
 public class InterleavingString {
     private boolean isInterleaveUtil(String s1, String s2, String s3, Map<String, Boolean> dp) {
-        if (s1.length() == 0 && s2.length() == 0 && s3.length() == 0)
+        if (s1.isEmpty() && s2.isEmpty() && s3.isEmpty())
             return true;
 
-        if (s3.length() == 0)
+        if (s3.isEmpty())
             return false;
 
         String key = s1 + "," + s2 + "," + s3;
 
         if (!dp.containsKey(key)) {
-            boolean x = (s1.length() != 0 && s1.charAt(0) == s3.charAt(0)) &&
+            boolean x = (!s1.isEmpty() && s1.charAt(0) == s3.charAt(0)) &&
                     isInterleaveUtil(s1.substring(1), s2, s3.substring(1), dp);
 
-            boolean y = (s2.length() != 0 && s2.charAt(0) == s3.charAt(0)) &&
+            boolean y = (!s2.isEmpty() && s2.charAt(0) == s3.charAt(0)) &&
                     isInterleaveUtil(s1, s2.substring(1), s3.substring(1), dp);
 
             dp.put(key, x | y);
