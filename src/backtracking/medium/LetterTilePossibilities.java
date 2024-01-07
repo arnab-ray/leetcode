18152c0fa522f34522748b6874a694f9ec49af0d
Arnab Ray <arnab.ray@udaan.com>
2024-01-07 19:58:28 +0530

    save changes

--- src/backtracking/medium/LetterTilePossibilities.java
+++ src/backtracking/medium/LetterTilePossibilities.java
@@ -4,13 +4,15 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class LetterTilePossibilities {
-    private void numUtil(String tiles, String currentStr, Set<String> result, boolean[] visited) {
-        result.add(currentStr);
+    private void numUtil(String tiles, StringBuilder sb, Set<String> result, boolean[] visited) {
+        result.add(sb.toString());
 
         for (int i = 0; i < tiles.length(); i++) {
             if (!visited[i]) {
                 visited[i] = true;
-                numUtil(tiles, currentStr + tiles.charAt(i), result, visited);
+                sb.append(tiles.charAt(i));
+                numUtil(tiles, sb, result, visited);
+                sb.setLength(sb.length() - 1);
                 visited[i] = false;
             }
         }
@@ -19,7 +21,7 @@ public class LetterTilePossibilities {
     public int numTilePossibilities(String tiles) {
         Set<String> set = new HashSet<>();
         boolean[] visited = new boolean[tiles.length()];
-        numUtil(tiles, "", set, visited);
+        numUtil(tiles, new StringBuilder(), set, visited);
 
         return set.size() - 1;
     }
