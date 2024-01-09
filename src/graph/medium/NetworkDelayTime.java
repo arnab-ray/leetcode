ebed61b38d5251e10fd26b2b2a331dc674d0f938
Arnab Ray <arnab.ray@udaan.com>
2024-01-09 21:51:27 +0530

    save changes

--- src/graph/medium/NetworkDelayTime.java
+++ src/graph/medium/NetworkDelayTime.java
@@ -3,7 +3,7 @@ package graph.medium;
 import java.util.*;
 
 public class NetworkDelayTime {
-    class Node {
+    private static class Node {
         int i, cost;
         Node(int i, int cost) {
             this.i = i;
@@ -11,39 +11,30 @@ public class NetworkDelayTime {
         }
     }
 
-    public int networkDelayTime(int[][] times, int N, int K) {
-        List<Node>[] adjList = new ArrayList[N];
-        for (int i = 0; i < N; i++)
-            adjList[i] = new ArrayList<>();
+    public int networkDelayTime(int[][] times, int n, int k) {
+        List<List<Node>> adjList = new ArrayList<>(n);
+        for (int i = 0; i < n; i++)
+            adjList.add(new ArrayList<>());
 
         for (int[] time : times) {
             int u = time[0] - 1;
             int v = time[1] - 1;
-            adjList[u].add(new Node(v, time[2]));
+            adjList.get(u).add(new Node(v, time[2]));
         }
 
-        for(int i = 0; i < adjList.length; i++) {
-            System.out.print(i + 1 + ": ");
-            for(Node v : adjList[i]) {
-                System.out.print(v.i + 1 + "->");
-            }
-            System.out.println();
-        }
-
-        boolean[] visited = new boolean[N];
-        int[] dist = new int[N];
+        boolean[] visited = new boolean[n];
+        int[] dist = new int[n];
         Arrays.fill(dist, Integer.MAX_VALUE);
 
         PriorityQueue<Node> queue = new PriorityQueue<>(Comparator.comparing(node -> node.cost));
-        queue.add(new Node(K - 1, 0));
-        visited[K - 1] = true;
-        dist[K - 1] = 0;
+        queue.add(new Node(k - 1, 0));
+        dist[k - 1] = 0;
 
         while (!queue.isEmpty()) {
             Node node = queue.poll();
             int u = node.i;
 
-            for(Node adjNode : adjList[u]) {
+            for(Node adjNode : adjList.get(u)) {
                 int v = adjNode.i;
                 int weight = adjNode.cost;
 
