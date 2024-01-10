70386d2cb7b0a54205d1a86aaf4b2b40fb20b284
Arnab Ray <arnab.ray@udaan.com>
2024-01-10 23:36:21 +0530

    save changes

--- src/arrays/medium/TrapRainWater.java
+++ src/arrays/medium/TrapRainWater.java
@@ -2,22 +2,17 @@ package arrays.medium;
 
 public class TrapRainWater {
     public int trap(int[] height) {
-        int[] leftMax= new int[height.length];
+        int[] leftMax = new int[height.length];
         int leftHigh = Integer.MIN_VALUE, rightHigh = Integer.MIN_VALUE;
 
         for(int i = 0; i < height.length; i++) {
-            if(height[i] > leftHigh) {
-                leftHigh = height[i];
-            }
+            leftHigh = Math.max(leftHigh, height[i]);
             leftMax[i] = leftHigh;
         }
 
         int water = 0;
         for(int i = height.length - 1; i >= 0; i--) {
-            if(height[i] > rightHigh) {
-                rightHigh = height[i];
-            }
-
+            rightHigh = Math.max(rightHigh, height[i]);
             water += (Math.min(leftMax[i], rightHigh) - height[i]);
         }
 
