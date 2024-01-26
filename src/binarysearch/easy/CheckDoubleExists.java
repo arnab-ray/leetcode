3385d347148e80a1efc0c92f0e0fd47a4ec7cb00
Arnab Ray <arnabray@Arnabs-MacBook-Pro.local>
2024-01-26 20:40:51 +0530

    minor refactor

--- src/binarysearch/easy/CheckDoubleExists.java
+++ src/binarysearch/easy/CheckDoubleExists.java
@@ -5,16 +5,16 @@ import java.util.Arrays;
 public class CheckDoubleExists {
     public boolean checkIfExist(int[] arr) {
         Arrays.sort(arr);
-        for(int i = 0; i < arr.length; i++){
+        for(int i = 0; i < arr.length; i++) {
             int j = binSearch(arr, 0, arr.length - 1, arr[i] * 2);
-            if(i != j && j != -1){
+            if(i != j && j != -1) {
                 return true;
             }
         }
         return false;
     }
 
-    private int binSearch(int[] A, int low, int high, int target){
+    private int binSearch(int[] A, int low, int high, int target) {
         if (low <= high) {
             int mid = low + (high - low) / 2;
             if(A[mid] == target)
