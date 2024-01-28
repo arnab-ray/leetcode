739a94f30c4d00e284438ff0d2d0559a1f8b8e74
Arnab Ray <arnabray@Arnabs-MacBook-Pro.local>
2024-01-28 19:34:25 +0530

    improve solution

--- src/backtracking/medium/PalindromePartitioning.java
+++ src/backtracking/medium/PalindromePartitioning.java
@@ -8,18 +8,18 @@ public class PalindromePartitioning {
     public List<List<String>> partition(String s) {
         List<List<String>> result = new LinkedList<>();
 
-        partitionUtil(s, 0, new LinkedList<>(), result);
+        backtrack(s, 0, new LinkedList<>(), result);
         return result;
     }
 
-    private void partitionUtil(String s, int pos, List<String> tempList, List<List<String>> result) {
+    private void backtrack(String s, int pos, List<String> tempList, List<List<String>> result) {
         if (pos == s.length()) {
             result.add(new ArrayList<>(tempList));
         } else {
             for (int i = pos; i < s.length(); i++) {
                 if (isPalindrome(s, pos, i)) {
                     tempList.add(s.substring(pos, i + 1));
-                    partitionUtil(s, i + 1, tempList, result);
+                    backtrack(s, i + 1, tempList, result);
                     tempList.remove(tempList.size() - 1);
                 }
             }
