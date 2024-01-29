b6b1e4d1dca1f60c88b6f8b84905ce43cafee9d0
Arnab Ray <arnabray@Arnabs-MacBook-Pro.local>
2024-01-29 18:19:37 +0530

    improve solution

--- src/tree/easy/SymmetricTree.java
+++ src/tree/easy/SymmetricTree.java
@@ -36,9 +36,7 @@ public class SymmetricTree {
     private boolean isIdentical(TreeNode root1, TreeNode root2) {
         if(root1 == null && root2 == null)
             return true;
-        if(root1 == null || root2 == null)
-            return false;
-        if(root1.val != root2.val)
+        if(root1 == null || root2 == null || root1.val != root2.val)
             return false;
         return isIdentical(root1.left, root2.left) && isIdentical(root1.right, root2.right);
     }
