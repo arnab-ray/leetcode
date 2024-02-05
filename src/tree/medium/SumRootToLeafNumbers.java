f7cba55062358c3b9e0c7b03c6c4c44728c369f9
Arnab Ray <arnabray@Arnabs-MacBook-Pro.local>
2024-02-05 18:43:38 +0530

    improve code

--- src/tree/medium/SumRootToLeafNumbers.java
+++ src/tree/medium/SumRootToLeafNumbers.java
@@ -1,9 +1,5 @@
 package tree.medium;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-
 public class SumRootToLeafNumbers {
     public class TreeNode {
       int val;
@@ -18,32 +14,17 @@ public class SumRootToLeafNumbers {
       }
     }
 
-    private void sumPathUtil(TreeNode root, List<Integer> path, AtomicInteger sum) {
-        path.add(root.val);
-        if(root.left == null && root.right == null) {
-            int pathSum = 0;
-            for(int i = path.size() - 1; i >= 0; i--) {
-                pathSum += (int) (Math.pow(10, path.size() - i - 1) * path.get(i));
-            }
-            sum.set(sum.get() + pathSum);
-        }
+    private int sumPathUtil(TreeNode root, int sum) {
+        if (root == null) return 0;
 
-        if(root.left != null)
-            sumPathUtil(root.left, path, sum);
-        if(root.right != null)
-            sumPathUtil(root.right, path, sum);
+        sum = (sum * 10) + root.val;
+        if (root.left == null && root.right == null)
+            return sum;
 
-        path.remove(path.size() - 1);
+        return sumPathUtil(root.left, sum) + sumPathUtil(root.right, sum);
     }
  
     public int sumNumbers(TreeNode root) {
-        if(root == null)
-            return 0;
-
-        List<Integer> path = new LinkedList<>();
-        AtomicInteger sum = new AtomicInteger(0);
-        sumPathUtil(root, path, sum);
-
-        return sum.get();
+        return sumPathUtil(root, 0);
     }
 }
